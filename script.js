4e07166caeaf7fd944f82f30e448e4f881f8d1ef
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-08-26 14:45:44 -0700

    Write private variables for GameBoard module
    A sanity check to make sure I know how modules work

---
+++ script.js
@@ -0,0 +1,36 @@
+/* ========================================================================== */
+/* Factory functions & IIFE modules */
+/* ========================================================================== */
+
+const gameBoard = (function () {
+    const grid = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ];
+    const winnerConfigs = [
+        // horizontals
+        [true , true , true , false, false, false, false, false, false], 
+        [false, false, false, true , true , true , false, false, false], 
+        [false, false, false, false, false, false, true , true , true ], 
+        // verticals
+        [true , false, false, true , false, false, true , false, false], 
+        [false, true , false, false, true , false, false, true , false], 
+        [false, false, true , false, false, true , false, false, true ], 
+        // diagonals
+        [true , false, false, false, true , false, false, false, true ], 
+        [false, false, true , false, true , false, true , false, false]
+    ];
+    let p1Mark;
+    let p2Mark;
+
+    return {};
+})();
+
+/* ========================================================================== */
+/* TESTING */
+/* ========================================================================== */
+
+function log(thing) {
+    console.log(thing);
+}
