dddfc691bb637e98cf96e23be445152d3bb7b131
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-08-27 13:14:37 -0700

    Add extra error case for GameBoard.placeMark

--- script.js
+++ script.js
@@ -22,20 +22,35 @@ const gameBoard = (function () {
         [false, false, true , false, true , false, true , false, false]
     ];
     let numMarks = 0;
+    const MARK_X = "x";
+    const MARK_O = "o";
 
+    /**
+     * 
+     * @param {Number} i 
+     * @param {Number} j 
+     * @returns Boolean
+     */
     const isBlank = function (i, j) {
         if (i < 0 || j < 0 || i > 2 || j > 2)
-            throw Error("Grid indices must be between 0 and 2, inclusive")
+            throw Error("Grid indices must be between 0 and 2, inclusive");
 
         return grid[i][j] === null;
     };
     
+    /**
+     * 
+     * @param {Number} i 
+     * @param {Number} j 
+     * @param {String} mark 
+     */
     const placeMark = function (i, j, mark) {
         if (i < 0 || j < 0 || i > 2 || j > 2)
-            throw Error("Grid indices must be between 0 and 2, inclusive")
-        if (!isBlank(i, j)) {
+            throw Error("Grid indices must be between 0 and 2, inclusive");
+        if (mark !== MARK_X && mark !== MARK_O) 
+            throw Error(`Mark must be either '${MARK_X}' or '${MARK_O}'`);
+        if (!isBlank(i, j)) 
             throw Error(`Grid cell (${i}, ${j}) is not blank`);
-        }
 
         grid[i][j] = mark;
         numMarks++;
