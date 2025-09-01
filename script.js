f128db165dddac113d73751051b8509859b45580
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-09-01 12:02:45 -0700

    Improve gameDisplay logic separation & readability
    More emphasis is placed on gameDisplay.update() to update alert messages
    by providing it an optional message parameter. This change aims to
    decouple the internal logic of calling gameControl and the external
    logic of updating the display.
    
    What prevents this from being called a refactor is that additional calls
    to update() is less performant and has redundancy: it is called even
    when the gameboard array has not changed, such as the case of clicking
    on an occupied cell. This can be optimized by adding a flag argument to
    update() that determines whether the game array should be rescanned.
    
    Several other changes were made for better readability & brevity,
    including clearer variable names, reordering, and restructuring.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 /**
  * This module is to be controlled by a game controller that should supply 
  * correct inputs in a valid order as defined by the rules of tic-tac-toe. 
- * The only error-checking this module provides is not allowing a mark to be
- * placed in an already occupied cell. 
+ * 
+ * Getter methods are provided for the valid marks and game states that are 
+ * possible, for usage in gameControl and displayControl modules. 
  */
 const gameBoard = (function () {
     const grid = [
@@ -274,22 +275,42 @@ const gameControl = (function () {
     };
 })();
 
-// @TODO refactor this whole module because it kind of got out of hand
+
 const gameDisplay = (function () {
     const [MARK_X, MARK_O] = gameBoard.getValidMarks();
     const boardStates = gameBoard.getPossibleStates();
 
+    /* ====================================================================== */
+    /* Module functions */
+    /* ====================================================================== */
 
-    const grid = document.querySelector("#ttt-grid");
-    const cells = grid.querySelectorAll(".ttt-cell");
-    const alert = document.querySelector("#alert-container");
-    cells.forEach((cell) => {
-        cell.addEventListener("click", handleCellClick); // (note)
-    }); 
-    /*  Note: despite my reluctant to add event listeners to each cell (not as 
-    performant as adding one only to the parent grid), I did it because 
-    otherwise I'm unsure how to ensure the cell receives the click event when I 
-    have an SVG overshadowing the cell that I wish to target. */
+    /**
+     * Update the grid display based on the game's internals, as provided
+     * by the gameBoard & gameControl modules. 
+     * An optional alert message (default: null) can be displayed, in case of 
+     * user requesting prohibited actions. 
+     * @param {String | undefined} alertMsg 
+     */
+    function update(alertMsg = null) {
+        alert.textContent = alertMsg;
+
+        const gameArray = gameBoard.getGridCopy();
+        gameArray.forEach((row, i) => {
+            row.forEach((content, j) => {
+                const cell = grid.querySelector(`[data-i="${i}"][data-j="${j}"]`);
+                drawContent(cell, content);
+            });
+        });
+
+        turnIndicator.textContent = gameControl.getNextMark();
+    }
+
+    /* ====================================================================== */
+    /* Add event listeners to DOM elements */
+    /* ====================================================================== */
+
+    /* Player data processing =============================================== */
+    /* ====================================================================== */
 
     const playersButton = document.querySelector("#open-create-players");
     const playersDialog = document.querySelector("#create-players-dialog");
@@ -301,124 +322,118 @@ const gameDisplay = (function () {
     const restartButton = document.querySelector("#restart-game");
     playersForm.addEventListener("submit", (evt) => {
         if (evt.submitter.id === "create-players-btn") {
-            // Use the FormData constructor since the form method ("dialog") 
-            // does not transmit the data via a formdata event
-            const formData = new FormData(playersForm);
+            // Retrieve the player names and create the players
+            const formData = new FormData(playersForm); // (note)
             const [p1name, p2name] = [formData.get("p1name"), formData.get("p2name")];
             gameControl.createPlayers(p1name, p2name); 
+
             gameControl.playGame();
             update();
 
-            playersButton.parentElement.removeChild(playersButton);
+            // Replace the "Start Game" button with the "Restart Game" button
+            playersButton.parentElement.removeChild(playersButton); 
             restartButton.style["display"] = "revert";
-            alert.textContent = "";
         }
 
         playersForm.reset(); 
     });
+    // (note) form method "dialog" does not naturally transmit data, thus 
+    // using the FormData constructor
+
     playersForm.addEventListener("formdata", (evt) => {
         const formData = evt.formData;
         formData.set("p1name", formData.get("p1name").toUpperCase());
         formData.set("p2name", formData.get("p2name").toUpperCase());
     });
 
+    /* Restart button ======================================================= */
+    /* ====================================================================== */
+
+
     restartButton.addEventListener("click", (evt) => {
         gameControl.endGame();
         gameControl.playGame();
-        update();
-        alert.textContent = "Game has restarted!";
+        update("Game has restarted!");
     });
 
+    /* The main playing field =============================================== */
+    /* ====================================================================== */
+
+    const grid = document.querySelector("#ttt-grid");
+    const cells = grid.querySelectorAll(".ttt-cell");
+    const alert = document.querySelector("#alert-container");
     const turnIndicator = document.querySelector("#turn-container > span");
-    function update() {
-        const gameArray = gameBoard.getGridCopy();
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                const cell = grid.querySelector(`[data-i="${i}"][data-j="${j}"]`);
-                const mark = gameArray[i][j];
-                putMark(cell, gameArray[i][j]);
-            }
-        }  
-        turnIndicator.textContent = gameControl.getNextMark();
-    }
 
+    cells.forEach((cell) => {
+        cell.addEventListener("click", handleCellClick); 
+        // Event listener added to each cell because I'm unsure how to use
+        // event delegation here when I have SVGs overlapping the cells
+    }); 
 
-    // @NOTE: might have to do better error handling, perhaps through some error 
-    // messages that gameControl can return for the display to print.
     function handleCellClick(evt) {
-        const cellElement = evt.currentTarget;
         if (!gameControl.hasGameBegun()) {
-            alert.textContent = "A game has not begun yet!";
+            update("A game has not begun yet!");
             return;
         }
+        
+        const cellElement = evt.currentTarget;
+        const [i, j] = [
+            cellElement.getAttribute("data-i"), 
+            cellElement.getAttribute("data-j")
+        ].map(Number);
+        const gameState = gameControl.playTurn(i, j);
+        update(!gameState ? "That spot is already filled" : undefined)
 
-        alert.textContent = ""; // Clear any prior alert message
-        const [i, j] = [cellElement.getAttribute("data-i"), 
-            cellElement.getAttribute("data-j")].map(Number);
-        const gameStatus = gameControl.playTurn(i, j);
-        if (!gameStatus){
-            alert.textContent = "That spot is already filled";
-            return;
-        }
-
-        update();
-        if (gameStatus === boardStates.ongoing) {
-            return;
-        }
-
-        handleGameEnd(gameStatus);
+        if (gameState === boardStates.xWin || 
+            gameState === boardStates.oWin ||
+            gameState === boardStates.tie 
+        )
+            displayResults(gameState);
     }
 
     const resultsDialog = document.querySelector("#results-dialog");
-    function handleGameEnd(gameStatus) {
-        const line1 = resultsDialog.querySelector("#results-line-1");
-        const line2 = resultsDialog.querySelector("#results-line-2");
-
+    const conclusionDisplay = resultsDialog.querySelector("#conclusion");
+    const scoreDisplay = resultsDialog.querySelector("#score");
+    
+    function displayResults(gameState) {
         const {player1: p1Data, player2: p2Data} = gameControl.getPlayerData();
-        let statusMsg;
-        switch (gameStatus) {
-            case boardStates.xWin: statusMsg =
-                `Player "${p1Data.name}" (${MARK_X}) has won the game!`;
-                break;
-            case boardStates.oWin: statusMsg =
-                `Player "${p2Data.name}" (${MARK_O}) has won the game!`;
-                break;
-            case boardStates.tie: statusMsg = 
-                `The game is a tie!`;
-                break;
+        const conclusionMsg = ((state) => {
+            switch (state) {
+                case boardStates.xWin: 
+                    return `Player "${p1Data.name}" (${MARK_X}) has won the game!`;
+                case boardStates.oWin: 
+                    return `Player "${p2Data.name}" (${MARK_O}) has won the game!`;
+                case boardStates.tie:  
+                    return "The game is a tie!";
+            }
+        })(gameState);
 
-        }
-
-        resultsDialog.showModal();
-        line1.textContent = statusMsg;
-        line2.textContent = 
+        conclusionDisplay.textContent = conclusionMsg;
+        scoreDisplay.textContent = 
             `Score: "${p1Data.name}" ${p1Data.score} | ${p2Data.score} "${p2Data.name}"`;
+        resultsDialog.showModal();
     }
 
     const playAgainBtn = resultsDialog.querySelector("#play-again-btn");
     playAgainBtn.addEventListener("click", (evt) => {
         resultsDialog.close();
-        gameControl.endGame();
+
         gameControl.playGame();
         update();
-        alert.textContent = "";
     });
 
-    function putMark(cell, mark) {
-        switch (mark) {
+    function drawContent(cell, content) {
+        switch (content) {
+            case null: cell.textContent = "";
+                break;
             case MARK_X: cell.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="2 2 18 18"><title>multiply</title><path d="M16 16H14V15H13V14H12V13H10V14H9V15H8V16H6V14H7V13H8V12H9V10H8V9H7V8H6V6H8V7H9V8H10V9H12V8H13V7H14V6H16V8H15V9H14V10H13V12H14V13H15V14H16Z" fill="black"/></svg>`;
                 break;
             case MARK_O: cell.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="-4 -4 30 30"><title>circle</title><path d="M15 1V2H17V3H18V4H19V5H20V7H21V15H20V17H19V18H18V19H17V20H15V21H7V20H5V19H4V18H3V17H2V15H1V7H2V5H3V4H4V3H5V2H7V1H15M14 3H8V4H6V5H5V6H4V8H3V14H4V16H5V17H6V18H8V19H14V18H16V17H17V16H18V14H19V8H18V6H17V5H16V4H14V3Z" fill="black"/></svg>`;
-                break;
-            case null: cell.textContent = "";
                 break;
         }
     }
 
 
-    return {
-        update //@TODO might remove
-    };
 })();
 
 /* 
